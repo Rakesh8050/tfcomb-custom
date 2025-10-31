331c24506d2d00e98cc1f604eab6e720ebcbfb75
Rakesh Kumar A <125812375+Rakesh8050@users.noreply.github.com>
2025-10-31 22:20:44 +0530

    Modify setup.py for tfcomb-custom configuration
    Updated setup.py for custom TF-COMB with enhanced features and fixed dependencies.

--- setup.py
+++ setup.py
@@ -3,86 +3,98 @@ import sys
 import re
 from setuptools import setup, Extension, dist
 
-# Test if numpy is installed
+# --- Ensure NumPy is available ---
 try:
     import numpy as np
-except:
-    # Else, fetch numpy if needed
+except ImportError:
     dist.Distribution().fetch_build_eggs(['numpy'])
     import numpy as np
 
-# Add cython modules depending on the availability of cython
+# --- Optional Cython compilation ---
 cmdclass = {}
 try:
     from Cython.Distutils import build_ext
     cmdclass = {'build_ext': build_ext}
+    use_cython = True
 except ImportError:
     use_cython = False
-else:
-    use_cython = True
 
-# To compile or not to compile
+# --- Define extensions ---
 if use_cython:
-    ext_modules = [Extension("tfcomb.counting", ["tfcomb/counting.pyx"], include_dirs=[".", np.get_include()])]
+    ext_modules = [
+        Extension(
+            "tfcomb.counting",
+            ["tfcomb/counting.pyx"],
+            include_dirs=[".", np.get_include()]
+        )
+    ]
 else:
     if os.path.exists("tfcomb/counting.c"):
-        ext_modules = [Extension("tfcomb.counting", ["tfcomb/counting.pyx"], include_dirs=[".", np.get_include()])]
-    else:  # Only happens when installing directly from source
-        sys.exit("Cython is needed to compile TF-comb.")
+        ext_modules = [
+            Extension(
+                "tfcomb.counting",
+                ["tfcomb/counting.c"],
+                include_dirs=[".", np.get_include()]
+            )
+        ]
+    else:
+        sys.exit("Cython is required to compile TF-COMB from source.")
 
+# --- Version finder ---
 def find_version(init_file):
-    version_file = open(init_file).read()
+    with open(init_file) as f:
+        version_file = f.read()
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    else:
-        raise RuntimeError("Unable to find version string.")
+    raise RuntimeError("Unable to find version string.")
 
-
-# Readme from git
+# --- Read long description ---
 def readme():
-    with open('README.md') as f:
+    with open("README.md", encoding="utf-8") as f:
         return f.read()
 
-setup(name='TF-COMB',
-        version=find_version(os.path.join("tfcomb", "__init__.py")),   # get version from __init__.py
-        description='Transcription Factor Co-Occurrence using Market Basket analysis',
-        long_description=readme(),
-        long_description_content_type='text/markdown',
-        url='',
-        author='Mette Bentsen',
-        author_email='mette.bentsen@mpi-bn.mpg.de',
-        license='MIT',
-        packages=['tfcomb'],
-        ext_modules=ext_modules,
-        cmdclass=cmdclass,
-        python_requires='>=3,',
-        setup_requires=['numpy'],
-        install_requires=[
-            'numpy',
-            'scipy',
-            'pysam',
-            'matplotlib>=2',
-            'pandas>=2.3.0',  # due to Issue#77
-            'tobias>=0.11',
-            'networkx>=2.4',  # G.node -> G.nodes
-            'python-louvain',
-            'graphviz',
-            'statsmodels',
-            'goatools',
-            'uropa',
-            'qnorm',
-            'dill',
-            'seaborn',
-            'tqdm',
-            'IPython'
-        ],
-        classifiers=[
-            'License :: OSI Approved :: MIT License',
-            'Intended Audience :: Science/Research',
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
-            'Programming Language :: Python :: 3'
-        ],
-        zip_safe=False,  # gives cython import error if True
-        include_package_data=True
-        )
+# --- Setup configuration ---
+setup(
+    name='tfcomb-custom',
+    version=find_version(os.path.join("tfcomb", "__init__.py")),
+    description='Custom TF-COMB: Fixed pandas var_name error and enhanced TF co-occurrence analysis.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Rakesh8050/tfcomb-custom',
+    author='Rakesh Kumar (Modified from Mette Bentsen)',
+    author_email='',
+    license='MIT',
+    packages=['tfcomb'],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    python_requires='>=3.8',
+    setup_requires=['numpy'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pysam',
+        'matplotlib>=2',
+        'pandas>=2.3.0',   # fixed for var_name issue
+        'tobias>=0.11',
+        'networkx>=2.4',
+        'python-louvain',
+        'graphviz',
+        'statsmodels',
+        'goatools',
+        'uropa',
+        'qnorm',
+        'dill',
+        'seaborn',
+        'tqdm',
+        'IPython'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
